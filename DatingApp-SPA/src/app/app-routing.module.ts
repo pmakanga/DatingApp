d6635e7ab59e1fcbbb20b128225013808b99f5b5
Patrick <pmakanga@gmail.com.com>
2019-01-16 22:13:31 +0300

    End of Section7

--- DatingApp-SPA/src/app/app-routing.module.ts
+++ DatingApp-SPA/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
+import { AuthGuard } from './_guards/auth.guard';
 
-const routes: Routes = [];
+//   const routes: Routes = [
+//   { path: 'home', component: HomeComponent },
+//   { path: 'members', component: MemberListComponent, canActivate: [AuthGuard] },
+//   { path: 'messages', component: MessagesComponent },
+//   { path: 'lists', component: ListsComponent },
+//   { path: '**', redirectTo: 'home', pathMatch: 'full' },
+// ];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'members', component: MemberListComponent, canActivate: [AuthGuard] },
+      { path: 'messages', component: MessagesComponent },
+      { path: 'lists', component: ListsComponent },
+    ]
+  },
+
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -12,6 +12,11 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AlertifyService } from './_services/alertify.service';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
+
 
 
 
@@ -20,19 +25,23 @@ import { AlertifyService } from './_services/alertify.service';
       AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      MemberListComponent,
+      ListsComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserModule,
       AppRoutingModule,
       HttpClientModule,
       FormsModule,
-      BsDropdownModule.forRoot()
+      BsDropdownModule.forRoot(),
    ],
    providers: [
       AuthService,
       ErrorInterceptorProvider,
-      AlertifyService
+      AlertifyService,
+      AuthGuard
    ],
    bootstrap: [
       AppComponent
